d3798b2a2eab14d8d8f4a6d39bf528cfb4de2165
Service Web <sw@gssmail.com>
2024-03-11 12:38:22 -0500

    Commit changes in C:\Websites\Git\emarlow\4132\GCG_4132_Inv_Custom.sql

---
+++ GCG_4132_Inv_Custom.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_4132_Inv_Custom
+ * ========================================== */
+CREATE TABLE "GCG_4132_Inv_Custom"(
+ "PART" CHAR(20),
+ "LOCATION" CHAR(2),
+ "LastOnHand" NUMERIC(12,4));
+
+
