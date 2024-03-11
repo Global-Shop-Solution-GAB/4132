12428830ac905ed6a2e5426ba558ea8513f22a4c
Service Web <sw@gssmail.com>
2024-03-11 12:38:22 -0500

    Commit changes in C:\Websites\Git\emarlow\4132\GCG_4132_FRGHT_INFO.sql

---
+++ GCG_4132_FRGHT_INFO.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_4132_Frght_Info
+ * ========================================== */
+CREATE TABLE "GCG_4132_Frght_Info"(
+ "Customer" CHAR(6),
+ "UPS_Acct_No" CHAR(10),
+ "FedEx_Acct_No" CHAR(10),
+ "Third_Party_Fr_Acct" CHAR(10),
+ "Rebate_Participant" BIT NOT NULL );
+
+
