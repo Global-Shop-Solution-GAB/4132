8dac808ba992d16e0ef699f9bdad0fe9954860c3
Service Web <sw@gssmail.com>
2024-03-11 12:38:22 -0500

    Commit changes in C:\Websites\Git\emarlow\4132\GCG_4132_RoofType.sql

---
+++ GCG_4132_RoofType.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_4132_RoofType
+ * ========================================== */
+CREATE TABLE "GCG_4132_RoofType"(
+ "PART" CHAR(20),
+ "LOCATION" CHAR(2),
+ "ROOF" CHAR(30));
+
+
